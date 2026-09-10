35fa58a4b5188ee3e1c0be6f9366305065115ec1
Deepak Gaikwad <gaikwad.deepak23@gmail.com>
2026-09-10 11:26:39 +0530

    meeshi removed

--- tests/meesho.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('opens Meesho', async ({ page }) => {
-  await page.goto('https://www.meesho.com/');
-
-  await expect(page).toHaveURL(/meesho\.com/);
-});
